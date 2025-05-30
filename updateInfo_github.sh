7508214c88504440b990fea0e159f54220460a53
whiteshark209 <Petartijanic01@gmail.com>
2025-05-29 22:21:12 -0500

    Fix the login bug

--- updateInfo_github.sh
+++ updateInfo_github.sh
@@ -1,34 +1,20 @@
-git filter-branch --env-filter '
+git filter-branch -f --env-filter '
 CORRECT_NAME="whiteshark209"
 CORRECT_EMAIL="Petartijanic01@gmail.com"
 
 # Update committer details
-if [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_EMAIL" ]
-then
-    # If committer email is not correct, update both name and email
+if [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_EMAIL" ]; then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-elif [ "$GIT_COMMITTER_NAME" != "$CORRECT_NAME" ]
-then
-    # If committer email is correct but name is not, update only the name
+elif [ "$GIT_COMMITTER_NAME" != "$CORRECT_NAME" ]; then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
 fi
 
 # Update author details
-if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ]
-then
-    # If author email is not correct, update both name and email
+if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ]; then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-elif [ "$GIT_AUTHOR_NAME" != "$CORRECT_NAME" ]
-then
-    # If author email is correct but name is not, update only the name
+elif [ "$GIT_AUTHOR_NAME" != "$CORRECT_NAME" ]; then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
 fi
 ' --tag-name-filter cat -- --branches --tags
-
-
-
-
-CORRECT_NAME="whiteshark209"
-CORRECT_EMAIL="Petartijanic01@gmail.com"
